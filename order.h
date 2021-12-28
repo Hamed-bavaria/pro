04c6cda3340e6b2bd11f02b31a28230e16b7dbd7
Hamed-bavaria <96795687+Hamed-bavaria@users.noreply.github.com>
2021-12-28 23:32:00 +0200

    Create order.h

---
+++ order.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class order
+{
+public:
+	int OrderID;
+	int CustomerID;
+	string CustomerName;
+	int productId;
+	string prductName;
+
+	void SetOrderID();
+	void SetCustomerID();
+	void SetCustomerName();
+	void SetproductId();
+	void SetprductName();
+	
+};
+
