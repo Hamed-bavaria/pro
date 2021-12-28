10bdcab84177f8ec2e5b779202cb1902ad3d42b9
Hamed-bavaria <96795687+Hamed-bavaria@users.noreply.github.com>
2021-12-29 00:02:44 +0200

    Create shoppingCart.h

---
+++ shoppingCart.h
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class shoppingCart
+{
+public:
+	void customerId();
+	int numberOfItems;
+	int calculateCost;
+	int checkout;
+	string viewAddress;
+	int phoneNumber;
+	string email;
+
+
+	void customerId();
+	void viewOrders();
+	void numberOfItems();
+	void editOreder();
+	void addOrder();
+	void calculateCost();
+	void checkout();
+	void viewAddress();
+	void phoneNumber();
+	void email();
+	void display();
+	void pack();
+	void unpack();
+	void displayAll();
+
+};
+
